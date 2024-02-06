8d4d59dc9553de5e9b43491e4813f299f6943f15
Anish Gupta <gupta.anish82@yahoo.com>
2024-02-06 20:32:46 +0100

    added new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Will you be my valentine?</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
